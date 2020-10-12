5906005df50506a8ba18446a826194d351edcac9
victorgg1993 <vigomga1993@gmail.com>
2020-10-12 20:20:57 +0200

    ejercicio 7

---
+++ ejercicio7/script.js
@@ -0,0 +1,12 @@
+
+
+function ejercicio7() {
+    let driver1 = "bcm43142a0";
+    let driver2 = window.prompt("Nombre del navegador: ");
+
+    console.log("El nombre del driver es ", driver1);
+    console.log("El nombre del navegador es ", driver2);
+
+    window.alert("El nombre del driver es " + driver1 + "\n"
+        + "El nombre del navegador es " + driver2);
+}
